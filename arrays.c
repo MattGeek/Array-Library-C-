435d57df529d046e4819fc79bd9145b9a274e2e3
MattGeek <solecustode109@gmail.com>
2017-12-14 10:15:53 +0100

    Uploaded the library - Version 2.3 (20171213)

---
+++ arrays.c
@@ -0,0 +1,204 @@
+/* ******************************************************************* *\
+ *  Copyright ©2017 Mattia D'ambrosio                                  *
+ *  ALL RIGHTS RESERVED!                                               *
+ *  Note: this library can only be used for educational purposes!      *
+ *                                                                     *
+ *  Version: 2.3 (20171213)                                            *
+\* ******************************************************************* */
+
+#include "arrays.h"
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/** \brief Caricamento di un vettore chiedendone i valori da caricare all'utente
+ *
+ * \param vet[] Vettore da caricare (passato per indirizzo)
+ * \param size Dimensione del vettore da caricare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void loadArray1D(int vet[], int size){
+    int i;
+    for (i=0;i<size;i++) {
+        printf("Inserisci il %d^ numero: ", i + 1);
+        scanf("%d", &vet[i]);
+    }
+}
+
+/** \brief Caricamento di un vettore con numeri generati casualmente, in un range definito dall'utente
+ *
+ * \param vet[] Vettore da caricare (passato per indirizzo)
+ * \param size Dimensione del vettore da caricare (passato per valore)
+ * \param max Numero più grande da generare (passato per valore)
+ * \param min Numero più piccolo da generare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void loadArray1DRandomly(int vet[], int size, int max, int min){
+    srand(time(NULL));
+    int i;
+    for (i=0;i<size;i++) {
+        vet[i]=rand()%max+min;
+    }
+}
+
+/** \brief Stampa di un vettore
+ *
+ * \param vet[] Vettore da stampare (passato per indirizzo)
+ * \param size Dimensione del vettore da stampare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void printArray1D(int vet[], int size){
+    int i;
+    for(i=0;i<size;i++)
+        printf("%d\t", vet[i]);
+    printf("\n");
+
+}
+
+/** \brief Inizializza un vettore a 0
+ *
+ * \param vet[] Vettore da inizializzare (passato per indirizzo)
+ * \param size Dimensione del vettore da inizializzare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void resetArray1D(int vet[], int size){
+    int i;
+    for(i=0;i<size;i++)
+        vet[i]=0;
+}
+
+/** \brief Ordinamento crescente di un'array sfruttando l'algoritmo: Insertion Sort
+ *
+ * \param vet[] Vettore da ordinare (passato per indirizzo)
+ * \param size Dimensione del vettore da ordinare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void ascendingInsertionSort(int vet[], int size){
+    int i,j,t;
+    for (i=1;i<size;i++){
+        t=vet[i];
+        j=i-1;
+        while(j>=0&&vet[j]>t){
+            vet[j+1]=vet[j];
+            j--;
+        }
+        vet[j+1]=t;
+    }
+
+}
+
+/** \brief Ordinamento decrescente di un'array sfruttando l'algoritmo: Insertion Sort
+ *
+ * \param vet[] Vettore da ordinare (passato per indirizzo)
+ * \param size Dimensione del vettore da ordinare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void descendingInsertionSort(int vet[], int size){
+    int i,j,t;
+    for (i=1;i<size;i++){
+            t=vet[i];
+            j=i-1;
+            while(j>=0&&vet[j]<t){
+                  vet[j+1]=vet[j];
+                  j--;
+                  vet[j+1]=t;
+            }
+      }
+}
+
+/** \brief Ordinamento crescente di un'array sfruttando l'algoritmo: Bubble Sort
+ *
+ * \param vet[] Vettore da ordinare (passato per indirizzo)
+ * \param size Dimensione del vettore da ordinare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void ascendingBubbleSort(int vet[], int size){
+    int i,t;
+    bool continueToExchange;
+    do{
+        continueToExchange=false;
+        for (i=0;i<size-1;i++)
+            if(vet[i]>vet[i+1]){
+                t=vet[i];
+                vet[i]=vet[i+1];
+                vet[i+1]=t;
+                continueToExchange=true;
+            }
+    }while(continueToExchange);
+}
+
+/** \brief Ordinamento decrescente di un'array sfruttando l'algoritmo: Bubble Sort
+ *
+ * \param vet[] Vettore da ordinare (passato per indirizzo)
+ * \param size Dimensione del vettore da ordinare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void descendingBubbleSort(int vet[], int size){
+    int i,t;
+    bool continueToExchange;
+    do{
+        continueToExchange=false;
+        for (i=0;i<size-1;i++)
+            if(vet[i]<vet[i+1]){
+                t=vet[i];
+                vet[i]=vet[i+1];
+                vet[i+1]=t;
+                continueToExchange=true;
+            }
+    }while(continueToExchange);
+}
+
+/** \brief Ordinamento crescente di un'array sfruttando l'algoritmo: Select Sort
+ *
+ * \param vet[] Vettore da ordinare (passato per indirizzo)
+ * \param size Dimensione del vettore da ordinare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void ascendingSelectSort(int vet[], int size){
+    int i,j,t,min;
+    for (i=0;i<size-1;i++){
+         min=i;
+        for (j=i+1;j<size;j++)
+            if (vet[j]<vet[min])
+                min=j;
+        if (i!=min){
+            t=vet[min];
+            vet[min]=vet[i];
+            vet[i]=t;
+        }
+    }
+}
+
+/** \brief Ordinamento decrescente di un'array sfruttando l'algoritmo: Select Sort
+ *
+ * \param vet[] Vettore da ordinare (passato per indirizzo)
+ * \param size Dimensione del vettore da ordinare (passato per valore)
+ *
+ * \return N/A E' una procedura
+ **/
+void descendingSelectSort(int vet[], int size){
+   int i,j,t,max;
+   for (i=0;i<size-1;i++){
+         max=i;
+        for (j=i+1;j<size;j++)
+            if (vet[j]>vet[max])
+                max=j;
+        if (i!=max){
+            t=vet[max];
+            vet[max]=vet[i];
+            vet[i]=t;
+        }
+    }
+}
+
---
+++ arrays.h
@@ -0,0 +1,38 @@
+/* ******************************************************************* *\
+ *  Copyright ©2017 Mattia D'ambrosio                                  *
+ *  ALL RIGHTS RESERVED!                                               *
+ *  Note: this library can only be used for educational purposes!      *
+ *                                                                     *
+ *  Version: 2.3 (20171213)                                            *
+\* ******************************************************************* */
+
+
+///Caricamento array da parte dell'utente
+void loadArray1D(int *, int);
+
+///Caricamento array con numeri casuali
+void loadArray1DRandomly(int *, int, int, int);
+
+///Stampa array
+void printArray1D(int *, int);
+
+///Inizializzazione array
+void resetArray1D(int *, int);
+
+///Ordinamento array crescente Insertion Sort
+void ascendingInsertionSort(int *, int);
+
+///Ordinamento array decrescente Insertion Sort
+void descendingInsertionSort(int *, int);
+
+///Ordinamento array crescente Bubble Sort
+void ascendingBubbleSort(int *, int);
+
+///Ordinamento array decrescente Bubble Sort
+void descendingBubbleSort(int *, int);
+
+///Ordinamento array crescente Select Sort
+void ascendingSelectSort(int *, int);
+
+///Ordinamento array decrescente Select Sort
+void descendingSelectSort(int *, int);
